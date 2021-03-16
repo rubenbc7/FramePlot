98531ac950448997ece4cbd9d28c354a640a9ff3
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-03-15 21:06:59 -0700

    steve y slime

---
+++ readframes.py
@@ -0,0 +1,32 @@
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+steve = wave.open('steve.wav', 'r')
+slime = wave.open('slime.wav', 'r')
+
+
+framesGM = steve.readframes(-1)
+framesAN = slime.readframes(-1)
+ondaconvertidaGM = np.frombuffer(framesGM, dtype='int16')
+ondaconvertidaAN = np.frombuffer(framesAN, dtype='int16')
+
+#print(frames[:10])
+
+framerate_gm = steve.getframerate()
+print (framerate_gm)
+time_gm = np.linspace(start=0, stop=len(ondaconvertidaGM)/framerate_gm, num=len(ondaconvertidaGM))
+print(time_gm[:10])
+
+framerate_AN = slime.getframerate()
+print (framerate_AN)
+time_AN = np.linspace(start=0, stop=len(ondaconvertidaAN)/framerate_AN, num=len(ondaconvertidaAN))
+print(time_AN[:10])
+
+plt.title('Good morning vs Good slime')
+
+plt.xlabel('Tiempo (segundos')
+plt.ylabel('Amplitud')
+
+plt.plot(time_gm, steve, label='steve')
+plt.plot(time_AN, slime, label='slime', alpha=0.5)
